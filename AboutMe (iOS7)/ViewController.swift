9495cdc4352179ff1184c94b8fe062450adcd075
Johnny Hicks <johnnydhicks@gmail.com>
2019-06-04 10:16:05 -0600

    Added another print statement

--- AboutMe (iOS7)/ViewController.swift
+++ AboutMe (iOS7)/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("My name is Johnny Hicks")
+        print("Here is another change")
     }
 
 
